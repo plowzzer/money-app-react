b78ae54091b69b6f07e8363f6e9e1607519b6d9b
Pedro <poloo.p3@gmail.com>
2019-01-20 19:28:40 -0200

    footer and another commons

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Pedro Pizzo
+      <a href="https://pedropizzo.com">Site</a>
+    </strong>
+  </footer>
+)
