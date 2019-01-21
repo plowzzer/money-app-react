0416b0edb3afa9c0bea78d7347c9ec8c4fa2daa9
Pedro <poloo.p3@gmail.com>
2019-01-20 23:50:05 -0200

    dashboard getting from server with redux

--- frontend/src/common/layout/grid.jsx
+++ frontend/src/common/layout/grid.jsx
@@ -7,8 +7,6 @@ export default class Grid extends Component {
     const cols = numbers ? numbers.split(' ') : []
     let classes = ''
 
-    console.log(cols)
-
     if(cols[0]) classes += `col-xs-${cols[0]} `
     if(cols[1]) classes += `col-sm-${cols[1]} `
     if(cols[2]) classes += `col-md-${cols[2]} `
