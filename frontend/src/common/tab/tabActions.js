83f9a00328fa6088a67aef480f6ba03f77aba529
Pedro <poloo.p3@gmail.com>
2019-01-21 22:40:18 -0200

    tabs into the billingCycles

---
+++ frontend/src/common/tab/tabActions.js
@@ -0,0 +1,5 @@
+export function selectTab(tabId) {
+  return {
+    type: 'TAB_SELECTED', payload: tabId
+  }
+}
---
+++ frontend/src/common/tab/tabContent.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import { bindActionsCreators } from 'redux'
+import { connect } from 'react-redux'
+
+class TabContent extends Component {
+  render() {
+    const selected = this.props.tab.selected === this.props.id
+    return(
+      <div id={this.props.id} className={`tab-pane ${selected ? 'active': ''}`}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return ({
+    tab: state.tab
+  })
+}
+export default connect(mapStateToProps)(TabContent)
