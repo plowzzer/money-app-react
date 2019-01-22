352aebb81a260f32be4c5692e0b8029bbf8f5c7b
Pedro <poloo.p3@gmail.com>
2019-01-22 13:22:50 -0200

    tabs visible or not

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -3,3 +3,11 @@ export function selectTab(tabId) {
     type: 'TAB_SELECTED', payload: tabId
   }
 }
+
+export function showTabs(...tabIds) {
+  const tabsToShow = {}
+  tabIds.forEach(e => tabsToShow[e] = true);
+  return {
+    type: 'TAB_SHOWED', payload: tabsToShow
+  }
+}
